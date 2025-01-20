366de59126f06c0565ef31e832698c35943d3114
Meghna Raghu <meghnathayyur@gmail.com>
2025-01-20 22:59:59 +0530

    Updated App.js with routing and added components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react'
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+
+import About from './components/About';
+import Contact from './components/Contact';
+import Project from './components/Project';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Header with Navigation Links */}
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">My Portfolio</Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link href="/">About</Nav.Link>
+            <Nav.Link href="/projects">Projects</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar>
+
+        {/* Main content */}
+        <Container>
+          <Switch>
+            <Route exact path="/" component={About} />
+            <Route path="/projects" component={Project} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
